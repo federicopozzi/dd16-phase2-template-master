85c4eb484db503f98cced3d99f7ed40d8d924332
Federico <federico.izzop@gmail.com>
2020-11-14 23:39:30 +0100

    add viz3 test

--- question-3/script.js
+++ question-3/script.js
@@ -1,16 +1,36 @@
-console.log("question 3 script is running!")
-Promise.all([d3.html("./question.html"), d3.html("./visualisation.svg")])
-.then(function([html, svgDocument]){
-    // populate page with content from question.html
-    const questionContent = d3.select(html).selectAll('body > *');
-    questionContent.each(function(d){
-        d3.select("#question-container").node().appendChild(this);
-    })
+// this is the Javascript file
+console.log("Script is loaded!");
+Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function([html, svgDocument]){
+  const questionContent = d3.select(html).selectAll('body > *');
+  questionContent.each(function(d){
+      d3.select("#question-container").node().appendChild(this);
+  });
+  let svgNode = svgDocument.querySelector("svg");
+  // console.log(svgNode);
+  let container = document.querySelector("#visualization");
+  // console.log(container);
+  container.appendChild(svgNode);
 
-    // Bring the visualization inside the page by using plain JS
-    let svgNode = svgDocument.querySelector("svg");
-    let container = document.querySelector("#visualisation");
-    container.appendChild(svgNode);
+//script gephi prova
+  // const posts = d3.selectAll("#posts_square");
+  // posts.style("opacity","0.4");
+  //
+  // const hover = d3.selectAll("#hover");
+  // hover.style("opacity","0");
+  //
+  // const tasto = d3.selectAll("#button");
+  // tasto.style("opacity","1");
+  //
+  // const verdi = d3.selectAll("#hashtag");
+  // verdi.style("opacity","0");
 
-    // Use d3js for interactions ...
-})
+//fade on mousehover
+  tasto.on("mouseover", function() {
+    console.log("ciao");
+    hover.style("opacity","1");
+  });
+  tasto.on("mouseout", function(){
+    hover.style("opacity","0");
+  })
+
+});
