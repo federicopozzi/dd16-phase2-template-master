b008f1b6b92ae09aa5a85c93edb4120fa3bc2706
Federico <federico.izzop@gmail.com>
2020-11-15 16:12:43 +0100

    Update script.js

--- question-3/script.js
+++ question-3/script.js
@@ -14,10 +14,10 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function(
   //script gephi prova
   //background
   const hash = d3.selectAll("#hashtag");
-  hash.style("opacity", "0.3");
+  hash.style("opacity", "1");
 
   const posts = d3.selectAll("#posts_square");
-  posts.style("opacity", "0.3");
+  posts.style("opacity", "1");
 
   //selectall nodes
   const clusters = d3.selectAll("#nodes > g");
@@ -26,10 +26,14 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function(
   //interaction on nodes
   clusters.on("mouseover", function(){
     d3.select(this).style("opacity", "1");
+    hash.style("opacity", "0.4");
+    posts.style("opacity", "0.4");
   })
 
   clusters.on("mouseout", function(){
     d3.select(this).style("opacity", "0");
+    hash.style("opacity", "1");
+    posts.style("opacity", "1");
   })
 
 
