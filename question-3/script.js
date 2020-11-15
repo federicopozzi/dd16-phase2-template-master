d17de3e820e5f7b2322a3249888912b92c46d961
Federico <federico.izzop@gmail.com>
2020-11-15 01:12:14 +0100

    interaction protocol3

--- question-3/script.js
+++ question-3/script.js
@@ -1,9 +1,9 @@
 // this is the Javascript file
 console.log("Script is loaded!");
-Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function([html, svgDocument]){
+Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function([html, svgDocument]) {
   const questionContent = d3.select(html).selectAll('body > *');
-  questionContent.each(function(d){
-      d3.select("#question-container").node().appendChild(this);
+  questionContent.each(function(d) {
+    d3.select("#question-container").node().appendChild(this);
   });
   let svgNode = svgDocument.querySelector("svg");
   // console.log(svgNode);
@@ -11,26 +11,37 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function(
   // console.log(container);
   container.appendChild(svgNode);
 
-//script gephi prova
-  // const posts = d3.selectAll("#posts_square");
-  // posts.style("opacity","0.4");
-  //
-  // const hover = d3.selectAll("#hover");
-  // hover.style("opacity","0");
-  //
-  // const tasto = d3.selectAll("#button");
-  // tasto.style("opacity","1");
-  //
-  // const verdi = d3.selectAll("#hashtag");
-  // verdi.style("opacity","0");
+  //script gephi prova
+  //background
+  const hash = d3.selectAll("#hashtag");
+  hash.style("opacity", "0.3");
 
-//fade on mousehover
-  tasto.on("mouseover", function() {
-    console.log("ciao");
-    hover.style("opacity","1");
-  });
-  tasto.on("mouseout", function(){
-    hover.style("opacity","0");
+  const posts = d3.selectAll("#posts_square");
+  posts.style("opacity", "0.3");
+
+  //selectall nodes
+  const clusters = d3.selectAll("#nodes > g");
+  clusters.style("opacity","0");
+
+  //interaction on nodes
+  clusters.on("mouseover", function(){
+    d3.select(this).style("opacity", "1");
   })
+
+  clusters.on("mouseout", function(){
+    d3.select(this).style("opacity", "0");
+  })
+
+
+
+
+  //fade on mousehover
+  // skull.on("mouseover", function() {
+  //   console.log("ciao");
+  //   skull.style("opacity","1");
+  // });
+  // skull.on("mouseout", function(){
+  //   skull.style("opacity","0");
+  // })
 
 });
