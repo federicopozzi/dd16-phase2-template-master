77462f6825914d94fa7e83985e32d1b4659f3013
Federico <federico.izzop@gmail.com>
2020-11-15 23:52:24 +0100

    Update script.js

--- question-1/script.js
+++ question-1/script.js
@@ -11,26 +11,7 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP1.svg")]).then(function(
   // console.log(container);
   container.appendChild(svgNode);
 
-//script gephi prova
-  // const posts = d3.selectAll("#posts_square");
-  // posts.style("opacity","0.4");
-  //
-  // const hover = d3.selectAll("#hover");
-  // hover.style("opacity","0");
-  //
-  // const tasto = d3.selectAll("#button");
-  // tasto.style("opacity","1");
-  //
-  // const verdi = d3.selectAll("#hashtag");
-  // verdi.style("opacity","0");
 
-//fade on mousehover
-  tasto.on("mouseover", function() {
-    console.log("ciao");
-    hover.style("opacity","1");
-  });
-  tasto.on("mouseout", function(){
-    hover.style("opacity","0");
-  })
+
 
 });
