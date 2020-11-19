0e2982023c3e752cf85cf10013d5261a6ed629aa
Federico <federico.izzop@gmail.com>
2020-11-19 01:37:41 +0100

    protocol 3

--- question-3/script.js
+++ question-3/script.js
@@ -14,6 +14,7 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function(
 
   // script gephi prova
   // background
+
   const hash = d3.selectAll("#hashtag");
   hash.style("opacity", "1");
 
@@ -27,13 +28,9 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function(
   clusters.style("opacity", "0");
 
 
-  //background
-  const background = d3.selectAll("#sfondo");
-  background.on("click", function() {
-    skull.style("opacity", "0");
-    nuke.style("opacity", "0");
-  })
 
+
+  d3.select("#targhette_nuove").style("opacity", "0");
 
 //hover sui nodes
   d3.select("#g_skull").on("mouseover", function() {
@@ -41,6 +38,7 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function(
     hash.style("opacity", "0.2");
     posts.style("opacity", "0.2");
     hashT.style("opacity", "0.2");
+    d3.select("#targhette_grandi").style("opacity", "0");
 
   });
   d3.select("#g_skull").on("mouseout", function() {
@@ -48,6 +46,7 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function(
     hash.style("opacity", "1");
     posts.style("opacity", "1");
     hashT.style("opacity", "1");
+    d3.select("#targhette_grandi").style("opacity", "1");
   });
 
   d3.select("#g_history").on("mouseover", function() {
@@ -55,32 +54,58 @@ Promise.all([d3.html("./question.html"), d3.html("./VizP3.svg")]).then(function(
     hash.style("opacity", "0.2");
     posts.style("opacity", "0.2");
     hashT.style("opacity", "0.2");
+    d3.select("#targhette_grandi").style("opacity", "0");
   });
   d3.select("#g_history").on("mouseout", function() {
     d3.select("#history").style("opacity", "0");
     hash.style("opacity", "1");
     posts.style("opacity", "1");
     hashT.style("opacity", "1");
+    d3.select("#targhette_grandi").style("opacity", "1");
   });
 
 
 
+  //background
+  const background = d3.selectAll("#sfondo");
+  background.on("click", function() {
+  d3.select("#nuclear").style("opacity", "0");
+  nuke.style("opacity", "0");
+  })
+
+
   //interaction on click sulle aree
-  const nuke = d3.selectAll("#nuclear");
+
+  const tutteFoto = d3.selectAll("#foto_feed > g");
+  tutteFoto.style("opacity", "0");
+  d3.select("#grid").style("opacity", "1");
+
+  const nuke = d3.selectAll("#area_nuclear_on");
   nuke.style("opacity", "0");
 
-  const skull = d3.selectAll("#death");
-  skull.style("opacity", "0");
-
-  const areaDeath = d3.selectAll("#area_death");
-  areaDeath.on("click", function() {
-    skull.style("opacity", "1");
-  })
-
-  const areaNuke = d3.selectAll("#area_nuclear");
-  areaNuke.on("click", function() {
+  nuke.on("click", function() {
     nuke.style("opacity", "1");
+    d3.select("#nuclear").style("opacity", "1");
   })
 
+  const death = d3.selectAll("#area_death_on");
+  death.style("opacity", "0");
 
+  const spanish = d3.selectAll("#area_spanish_on");
+  spanish.style("opacity", "0");
+
+  const horror = d3.selectAll("#area_horror_on");
+  horror.style("opacity", "0");
+
+  const halloween = d3.selectAll("#area_halloween_on");
+  halloween.style("opacity", "0");
+
+  const travel = d3.selectAll("#area_travel_on");
+  travel.style("opacity", "0");
+
+  const korea = d3.selectAll("#area_korean_on");
+  korea.style("opacity", "0");
+
+  const cambodian = d3.selectAll("#area_cambodian_on");
+  cambodian.style("opacity", "0");
 });
